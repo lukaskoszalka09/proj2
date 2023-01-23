8ce80c028cf3c449904adcd672f8fb05be85fbce
lukas <lukaskoszalka09@gmail.com>
2023-01-23 13:13:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/proj2/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.proj2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    boolean gameActive = true;
+    int Player = 0;
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+            {0, 4, 8}, {2, 4, 6}};
+    public static int counter = 0;
+    public void playerTap(View view) {
+        ImageView img = (ImageView) view;
+        int tappedImage = Integer.parseInt(img.getTag().toString());
+        if (!gameActive) {
+            gameReset(view);
+        }
+        if (gameState[tappedImage] == 2) {
+            counter++;
+            if (counter == 9) {
+                gameActive = false;
+            }
+            gameState[tappedImage] = Player;
+            img.setTranslationY(-1000f);
+            if (Player == 0) {
+                img.setImageResource(R.drawable.x);
+                Player = 1;
+                TextView status = findViewById(R.id.status);
+                status.setText("O's Turn - Tap to play");
+            } else {
+                img.setImageResource(R.drawable.o);
+                Player = 0;
+                TextView status = findViewById(R.id.status);
+                status.setText("X's Turn - Tap to play");
+            }
+            img.animate().translationYBy(1000f).setDuration(300);
+        }
+        int flag = 0;
+        for (int[] winPosition : winPositions) {
+            if (gameState[winPosition[0]] == gameState[winPosition[1]] &&
+                    gameState[winPosition[1]] == gameState[winPosition[2]] &&
+                    gameState[winPosition[0]] != 2) {
+                flag = 1;
+                String winnerStr;
+                gameActive = false;
+                if (gameState[winPosition[0]] == 0) {
+                    winnerStr = "X has won";
+                } else {
+                    winnerStr = "O has won";
+                }
+                TextView status = findViewById(R.id.status);
+                status.setText(winnerStr);
+            }
+        }
+        if (counter==9 && flag == 0) {
+            TextView status = findViewById(R.id.status);
+            status.setText("Match Draw");
+        }
+    }
+    public void gameReset(View view) {
+        gameActive = true;
+        Player = 0;
+        for (int i = 0; i < gameState.length; i++) {
+            gameState[i] = 2;
+        }
+        ((ImageView) findViewById(R.id.imageView0)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView1)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView2)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView3)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView4)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView5)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView6)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView7)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView8)).setImageResource(0);
+
+        TextView status = findViewById(R.id.status);
+        status.setText("X's Turn - Tap to play");
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //enabling fullscreen cuz nothing else worked
+        setContentView(R.layout.activity_main);
+
+    }
+}
